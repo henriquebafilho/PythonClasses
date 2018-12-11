652ecb0f79fcdfb121274ef7919bcbca5c4be9e8
Henrique Filho <hbaf71@gmail.com>
2018-12-11 19:51:34 -0200

    somando números

---
+++ 003 Somando números.py
@@ -0,0 +1,5 @@
+#Crie um programa que leia dois números e mostre a soma entre eles
+n1 = int(input('Insira o primeiro valor: '))
+n2 = int(input('Insira o segundo valor: '))
+soma = n1 + n2
+print('{} + {} = {}'.format(n1, n2, soma))
